08577ce65f8325e00077465d15bf1fa715f93dca
Xander Wessels <xanwes97@hotmail.com>
2022-10-22 21:06:49 +0200

    feature

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Please input a number.");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
+    let mut num1 = String::new();
+    
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut num1)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    println!("Please input a second number.");
 
+    let mut num2 = String::new();
+
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Failed to read line");
+
+    match num2.cmp(&num1) {
+        Ordering::Less => println!("Your first number is bigger!"),
+        Ordering::Greater => println!("Your second number is bigger!"),
+        Ordering::Equal => println!("Those numbers are the same!"),
+    }
 }
